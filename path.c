11203a61ffb7cdb0822d0c67c2def746bd6dc794
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-05-18 17:30:41 +0300

    Update path.c

--- path.c
+++ path.c
@@ -1,181 +1,143 @@
 #include "shell.h"
 
-/**
- * path_execute - executes a command in the path
- * @command: full path to the command
- * @vars: pointer to struct of variables
- *
- * Return: 0 on succcess, 1 on failure
- */
-int path_execute(char *command, vars_t *vars)
-{
-	pid_t child_pid;
+char *fill_path_dir(char *path);
+list_t *get_path_dir(char *path);
 
-	if (access(command, X_OK) == 0)
+/**
+ * get_location - Locates a command in the PATH.
+ * @command: The command to locate.
+ *
+ * Return: If an error occurs or the command cannot be located - NULL.
+ *         Otherwise - the full pathname of the command.
+ */
+char *get_location(char *command)
+{
+	char **path, *temp;
+	list_t *dirs, *head;
+	struct stat st;
+
+	path = _getenv("PATH");
+	if (!path || !(*path))
+		return (NULL);
+
+	dirs = get_path_dir(*path + 5);
+	head = dirs;
+
+	while (dirs)
 	{
-		child_pid = fork();
-		if (child_pid == -1)
-			print_error(vars, NULL);
-		if (child_pid == 0)
+		temp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
+		if (!temp)
+			return (NULL);
+
+		_strcpy(temp, dirs->dir);
+		_strcat(temp, "/");
+		_strcat(temp, command);
+
+		if (stat(temp, &st) == 0)
 		{
-			if (execve(command, vars->av, vars->env) == -1)
-				print_error(vars, NULL);
+			free_list(head);
+			return (temp);
+		}
+
+		dirs = dirs->next;
+		free(temp);
+	}
+
+	free_list(head);
+
+	return (NULL);
+}
+
+/**
+ * fill_path_dir - Copies path but also replaces leading/sandwiched/trailing
+ *		   colons (:) with current working directory.
+ * @path: The colon-separated list of directories.
+ *
+ * Return: A copy of path with any leading/sandwiched/trailing colons replaced
+ *	   with the current working directory.
+ */
+char *fill_path_dir(char *path)
+{
+	int i, length = 0;
+	char *path_copy, *pwd;
+
+	pwd = *(_getenv("PWD")) + 4;
+	for (i = 0; path[i]; i++)
+	{
+		if (path[i] == ':')
+		{
+			if (path[i + 1] == ':' || i == 0 || path[i + 1] == '\0')
+				length += _strlen(pwd) + 1;
+			else
+				length++;
 		}
 		else
+			length++;
+	}
+	path_copy = malloc(sizeof(char) * (length + 1));
+	if (!path_copy)
+		return (NULL);
+	path_copy[0] = '\0';
+	for (i = 0; path[i]; i++)
+	{
+		if (path[i] == ':')
 		{
-			wait(&vars->status);
-			if (WIFEXITED(vars->status))
-				vars->status = WEXITSTATUS(vars->status);
-			else if (WIFSIGNALED(vars->status) && WTERMSIG(vars->status) == SIGINT)
-				vars->status = 130;
-			return (0);
-		}
-		vars->status = 127;
-		return (1);
-	}
-	else
-	{
-		print_error(vars, ": Permission denied\n");
-		vars->status = 126;
-	}
-	return (0);
-}
-
-/**
- * find_path - finds the PATH variable
- * @env: array of environment variables
- *
- * Return: pointer to the node that contains the PATH, or NULL on failure
- */
-char *find_path(char **env)
-{
-	char *path = "PATH=";
-	unsigned int i, j;
-
-	for (i = 0; env[i] != NULL; i++)
-	{
-		for (j = 0; j < 5; j++)
-			if (path[j] != env[i][j])
-				break;
-		if (j == 5)
-			break;
-	}
-	return (env[i]);
-
-}
-
-/**
- * check_for_path - checks if the command is in the PATH
- * @vars: variables
- *
- * Return: void
- */
-void check_for_path(vars_t *vars)
-{
-	char *path, *path_dup = NULL, *check = NULL;
-	unsigned int i = 0, r = 0;
-	char **path_tokens;
-	struct stat buf;
-
-	if (check_for_dir(vars->av[0]))
-		r = execute_cwd(vars);
-	else
-	{
-		path = find_path(vars->env);
-		if (path != NULL)
-		{
-			path_dup = _strdup(path + 5);
-			path_tokens = tokenize(path_dup, ":");
-			for (i = 0; path_tokens && path_tokens[i]; i++, free(check))
+			if (i == 0)
 			{
-				check = _strcat(path_tokens[i], vars->av[0]);
-				if (stat(check, &buf) == 0)
-				{
-					r = path_execute(check, vars);
-					free(check);
-					break;
-				}
+				_strcat(path_copy, pwd);
+				_strcat(path_copy, ":");
 			}
-			free(path_dup);
-			if (path_tokens == NULL)
+			else if (path[i + 1] == ':' || path[i + 1] == '\0')
 			{
-				vars->status = 127;
-				new_exit(vars);
-			}
-		}
-		if (path == NULL || path_tokens[i] == NULL)
-		{
-			print_error(vars, ": not found\n");
-			vars->status = 127;
-		}
-		free(path_tokens);
-	}
-	if (r == 1)
-		new_exit(vars);
-}
-
-/**
- * execute_cwd - executes the command in the current working directory
- * @vars: pointer to struct of variables
- *
- * Return: 0 on success, 1 on failure
- */
-int execute_cwd(vars_t *vars)
-{
-	pid_t child_pid;
-	struct stat buf;
-
-	if (stat(vars->av[0], &buf) == 0)
-	{
-		if (access(vars->av[0], X_OK) == 0)
-		{
-			child_pid = fork();
-			if (child_pid == -1)
-				print_error(vars, NULL);
-			if (child_pid == 0)
-			{
-				if (execve(vars->av[0], vars->av, vars->env) == -1)
-					print_error(vars, NULL);
+				_strcat(path_copy, ":");
+				_strcat(path_copy, pwd);
 			}
 			else
-			{
-				wait(&vars->status);
-				if (WIFEXITED(vars->status))
-					vars->status = WEXITSTATUS(vars->status);
-				else if (WIFSIGNALED(vars->status) && WTERMSIG(vars->status) == SIGINT)
-					vars->status = 130;
-				return (0);
-			}
-			vars->status = 127;
-			return (1);
+				_strcat(path_copy, ":");
 		}
 		else
 		{
-			print_error(vars, ": Permission denied\n");
-			vars->status = 126;
+			_strncat(path_copy, &path[i], 1);
 		}
-			return (0);
 	}
-	print_error(vars, ": not found\n");
-	vars->status = 127;
-	return (0);
+	return (path_copy);
 }
 
 /**
- * check_for_dir - checks if the command is a part of a path
- * @str: command
+ * get_path_dir - Tokenizes a colon-separated list of
+ *                directories into a list_s linked list.
+ * @path: The colon-separated list of directories.
  *
- * Return: 1 on success, 0 on failure
+ * Return: A pointer to the initialized linked list.
  */
-int check_for_dir(char *str)
+list_t *get_path_dir(char *path)
 {
-	unsigned int i;
+	int index;
+	char **dirs, *path_copy;
+	list_t *head = NULL;
 
-	for (i = 0; str[i]; i++)
+	path_copy = fill_path_dir(path);
+	if (!path_copy)
+		return (NULL);
+	dirs = _strtok(path_copy, ":");
+	free(path_copy);
+	if (!dirs)
+		return (NULL);
+
+	for (index = 0; dirs[index]; index++)
 	{
-		if (str[i] == '/')
-			return (1);
+		if (add_node_end(&head, dirs[index]) == NULL)
+		{
+			free_list(head);
+			free(dirs);
+			return (NULL);
+		}
 	}
-	return (0);
+
+	free(dirs);
+
+	return (head);
 }
+
+			
 
